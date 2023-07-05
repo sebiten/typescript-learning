5be7a3f2699bfd50f2a590bb1afaf108fb430356
Sebiten <sebdevspace@gmail.com>
2023-07-05 10:51:04 -0300

    literaltypes- uniontypes

--- basics.ts
+++ basics.ts
@@ -1,4 +1,29 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resulConversion: string
+) {
   let result;
-  if
-}
+  if (typeof input1 === "number" && typeof input2 === "number" || resulConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+//   if (resulConversion === "as-number") {
+//     return +result;
+//   } else {
+//     return result.toString();
+//   }
+// }
+
+
+
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Seb", "as-text");
+console.log(combinedNames);
